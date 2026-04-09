168f2f1d9784c3b55f941acc6018eeb389a61c14
Hav0k94 <dev0k@hav0k.fr>
2026-04-09 14:15:47 +0200

    refactor(zsh): extract cfg alias, simplify bat style flags and add gc-dry alias for garbage collect dry run

--- modules/home-manager/zsh/alias.nix
+++ modules/home-manager/zsh/alias.nix
@@ -1,15 +1,20 @@
 { config, lib, ... }:
 
+let
+  cfg = config.myModules.zsh;
+in
+
 {
-  config = lib.mkIf config.myModules.zsh.enable {
+  config = lib.mkIf cfg.enable {
     programs.zsh.shellAliases = {
       # Better cat
-      cat = "bat --style header --style snip --style changes --style header";
+      cat = "bat --style=header,snip,changes";
       # NixOS rebuild
       update-wsl = "sudo nixos-rebuild switch --flake './#wsl'";
       update-vps = "sudo nixos-rebuild switch --flake './#vps'";
       # Garbage collect
       gc = "sudo nix-collect-garbage --delete-older-than 7d";
+      gc-dry = "sudo nix-collect-garbage --delete-older-than 7d --dry-run";
       # Alias nvim
       svim = "sudo -E nvim";
     };
