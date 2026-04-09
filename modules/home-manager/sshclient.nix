74f1cbc9aeee7959043c2b4c78eef30a8d89dda5
Hav0k94 <dev0k@hav0k.fr>
2026-04-09 11:42:32 +0200

    fix(ssh): resolve identity file path using homeDirectory

--- modules/home-manager/sshclient.nix
+++ modules/home-manager/sshclient.nix
@@ -28,7 +28,7 @@ in
           addKeysToAgent = "yes";  # s'applique à tous les hôtes
           serverAliveInterval = 60;  # évite les déconnexions sur sessions idle
           serverAliveCountMax = 3;
-          identityFile = "~/.ssh/id_ed25519";  # clé par défaut pour tous les hôtes
+          identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";  # clé par défaut pour tous les hôtes
         };
       };
     };
